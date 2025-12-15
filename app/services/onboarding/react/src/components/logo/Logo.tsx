1d18c350263ef21b7a865316aa36271405ea6694
IJssel <ijssel@synapt.nl>
2025-12-15 22:17:02 +0100

    Remove flicker animation from Logo component

--- app/services/onboarding/react/src/components/logo/Logo.tsx
+++ app/services/onboarding/react/src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import "./logo.less";
 
 export default function Logo() {
     return (
-        <div className={"d-flex mainLogo flicker flex-row justify-content-center align-items-center gap-2"}>
+        <div className={"d-flex mainLogo flex-row justify-content-center align-items-center gap-2"}>
             <MdDirectionsBike size={"2.5rem"} color={"white"}/>
             <h1 className={"m-0 text-white"}>BikeFla<MdFlashOn style={{height: "3rem", margin: "0 -0.5rem"}} className={"text-primary"}/>h</h1>
         </div>
