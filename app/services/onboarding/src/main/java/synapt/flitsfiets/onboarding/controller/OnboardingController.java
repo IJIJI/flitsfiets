080a81fc30668122c727732be006cef2da08af57
IJssel <ijssel@synapt.nl>
2025-12-17 18:18:59 +0100

    First data parse

--- app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
+++ app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
@@ -1,10 +1,13 @@
 package synapt.flitsfiets.onboarding.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
-import synapt.flitsfiets.common.enums.SubscriptionType;
+import synapt.flitsfiets.common.dto.user.creation.UserCreationDTO;
+import synapt.flitsfiets.common.enums.BikeType;
+import synapt.flitsfiets.common.enums.PlanType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +18,10 @@ public class OnboardingController {
 
     @PostMapping
 //    @GetMapping
-    public Map<String, Object> onboardUser() {
+    public Map<String, Object> onboardUser(@RequestBody UserCreationDTO requestedUser) {
+
+        System.out.println("User Requested:");
+        System.out.println(requestedUser);
 
         Map<String, Object> result = new HashMap<>();
 
@@ -23,7 +29,8 @@ public class OnboardingController {
         newUser.setId(123L);
         newUser.setName("Eik");
         newUser.setEmail("eik@test.com");
-        newUser.setActiveSubscription(SubscriptionType.ELECTRIC);
+        newUser.setActiveBikeType(BikeType.ELECTRIC);
+        newUser.setActivePlanType(PlanType.MONTHLY);
 
         result.put("user", newUser);
 
