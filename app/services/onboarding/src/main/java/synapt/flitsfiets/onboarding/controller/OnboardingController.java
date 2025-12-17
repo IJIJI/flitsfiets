3dbfe7ab5c535ca7c7bd6edad1b3b031b33a3c20
IJssel <ijssel@synapt.nl>
2025-12-17 16:36:26 +0100

    Added testuser

--- app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
+++ app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
@@ -1,9 +1,10 @@
 package synapt.flitsfiets.onboarding.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
+import synapt.flitsfiets.common.enums.SubscriptionType;
 
 @RestController
 @RequestMapping("/api/onboarding")
@@ -11,7 +12,13 @@ public class OnboardingController {
 
     @PostMapping
 //    @GetMapping
-    public long onboardUser() {
-        return 42L;
+    public UserExtendedDTO onboardUser() {
+
+        UserExtendedDTO newUser = new UserExtendedDTO();
+        newUser.setId(123L);
+        newUser.setName("Eik");
+        newUser.setActiveSubscription(SubscriptionType.ELECTRIC);
+
+        return newUser;
     }
 }
