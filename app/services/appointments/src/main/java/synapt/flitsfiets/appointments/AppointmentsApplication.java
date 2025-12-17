8c3de4d08e9c814a3ac8ee4f17892296df358bc9
IJssel <ijssel@synapt.nl>
2025-12-17 15:25:25 +0100

    Added shared properties to find containers and fixed jmx ports and container names

--- app/services/appointments/src/main/java/synapt/flitsfiets/appointments/AppointmentsApplication.java
+++ app/services/appointments/src/main/java/synapt/flitsfiets/appointments/AppointmentsApplication.java
@@ -2,8 +2,10 @@ package synapt.flitsfiets.appointments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "synapt.flitsfiets.common")
 public class AppointmentsApplication {
 
 	public static void main(String[] args) {
--- app/services/users/src/main/java/synapt.flitsfiets.users/UsersApplication.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/UsersApplication.java
@@ -2,10 +2,12 @@ package synapt.flitsfiets.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan(basePackages = "synapt.flitsfiets.common")
 public class UsersApplication {
 
 	public static void main(String[] args) {
