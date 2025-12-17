18d08bc215f6a36c0f65b5331d89c8b9b58242df
IJssel <ijssel@synapt.nl>
2025-12-17 16:04:49 +0100

    Added callback support

--- app/services/onboarding/react/src/components/form/multistep/MultiStepFormContainer.tsx
+++ app/services/onboarding/react/src/components/form/multistep/MultiStepFormContainer.tsx
@@ -3,12 +3,21 @@ import FormStep from "./FormStep.tsx";
 import FormContainer from "../FormContainer.tsx";
 import {MdArrowBack} from "react-icons/md";
 
-export default function MultiStepFormContainer({children, titles, completed}: { children: ReactElement<typeof FormStep>[], titles: string[], completed: boolean[] }) {
+export default function MultiStepFormContainer({children, titles, completed, callback, blockReturn}: { children: ReactElement<typeof FormStep>[], titles: string[], completed: boolean[], callback: [call:null|(() => void)], blockReturn: boolean[] }) {
 
     const steps = Children.toArray(children);
 
     const [currentStep, setCurrentStep] = useState(0);
-    // const [currentStep, setCurrentStep] = useState(steps[0]);
+
+    const nextstep = () => {
+        const currentCallback = callback[currentStep];
+
+        setCurrentStep(currentStep + 1);
+
+        if (currentCallback != null) {
+            currentCallback();
+        }
+    }
 
     return (
         <FormContainer className="multistep-form-container">
@@ -22,11 +31,11 @@ export default function MultiStepFormContainer({children, titles, completed}: { 
                 {children[currentStep]}
             </div>
             <div className="footer d-flex justify-content-between">
-                <div className={"backContainer "+(!(currentStep > 0) ? "hidden" : "")}>
+                <div className={"backContainer "+(!(currentStep > 0) || blockReturn[currentStep] ? "hidden" : "")}>
                     <button className="btn btn-secondary" onClick={() => setCurrentStep(currentStep - 1)} disabled={!(currentStep > 0)}><MdArrowBack /></button>
                 </div>
                 {currentStep < steps.length - 1 ?
-                    <button className="btn btn-primary w-100" onClick={() => setCurrentStep(currentStep + 1)} disabled={!(completed[currentStep])}>Next</button>
+                    <button className="btn btn-primary w-100" onClick={() => nextstep()} disabled={!(completed[currentStep])}>Next</button>
                     :
                     <button className="btn btn-primary w-100" type="submit">Submit</button>
                 }
