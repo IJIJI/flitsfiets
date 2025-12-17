3e26983efec3d0ccfa706619582ce3e5aace5908
IJssel <ijssel@synapt.nl>
2025-12-17 17:23:38 +0100

    Added map return

--- app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
+++ app/services/onboarding/src/main/java/synapt/flitsfiets/onboarding/controller/OnboardingController.java
@@ -6,19 +6,27 @@ import org.springframework.web.bind.annotation.RestController;
 import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
 import synapt.flitsfiets.common.enums.SubscriptionType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/onboarding")
 public class OnboardingController {
 
     @PostMapping
 //    @GetMapping
-    public UserExtendedDTO onboardUser() {
+    public Map<String, Object> onboardUser() {
+
+        Map<String, Object> result = new HashMap<>();
 
         UserExtendedDTO newUser = new UserExtendedDTO();
         newUser.setId(123L);
         newUser.setName("Eik");
+        newUser.setEmail("eik@test.com");
         newUser.setActiveSubscription(SubscriptionType.ELECTRIC);
 
-        return newUser;
+        result.put("user", newUser);
+
+        return result;
     }
 }
