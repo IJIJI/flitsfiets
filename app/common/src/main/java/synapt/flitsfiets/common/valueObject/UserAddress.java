d1f1c037bb818b81543b6ccea2271938a7261c30
IJssel <ijssel@synapt.nl>
2025-12-18 22:29:33 +0100

    Added streetnumber

--- app/common/src/main/java/synapt/flitsfiets/common/valueObject/UserAddress.java
+++ app/common/src/main/java/synapt/flitsfiets/common/valueObject/UserAddress.java
@@ -5,12 +5,14 @@ import lombok.Value;
 @Value
 public class UserAddress {
     String street;
+    String streetNumber;
     String city;
     String postalCode;
     String country;
 
-    public UserAddress(String street, String city, String postalCode, String country) {
+    public UserAddress(String street, String streetNumber, String city, String postalCode, String country) {
         this.street = street;
+        this.streetNumber = streetNumber;
         this.city = city;
         this.postalCode = postalCode;
         this.country = country;
