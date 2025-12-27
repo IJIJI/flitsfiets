796c4835aeae342e28bbcdd6302d4dc46b69ef31
IJssel <ijssel@synapt.nl>
2025-12-27 20:37:07 +0100

    Added modelmapper to mailing

--- app/services/mailing/src/main/java/synapt/flitsfiets/mailing/MailingApplication.java
+++ app/services/mailing/src/main/java/synapt/flitsfiets/mailing/MailingApplication.java
@@ -3,9 +3,12 @@ package synapt.flitsfiets.mailing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
+import synapt.flitsfiets.common.config.ModelMapperConfig;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = "synapt.flitsfiets.common")
+@Import({ModelMapperConfig.class})
 public class MailingApplication {
 
 	public static void main(String[] args) {
