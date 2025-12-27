9840ae736757180f34eaff2d370dea3c7259330b
IJssel <ijssel@synapt.nl>
2025-12-27 19:50:35 +0100

    Added publishing in userservice

--- app/services/users/src/main/java/synapt.flitsfiets.users/service/UserService.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/service/UserService.java
@@ -1,10 +1,13 @@
 package synapt.flitsfiets.users.service;
 
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
 import synapt.flitsfiets.common.dto.user.UserFullPasswordDTO;
+import synapt.flitsfiets.common.events.users.UserCreated;
+import synapt.flitsfiets.users.messaging.UsersEventPublisher;
 import synapt.flitsfiets.users.model.User;
 import synapt.flitsfiets.users.repository.UserRepository;
 
@@ -16,13 +19,25 @@ public class UserService
     @Autowired
     private ModelMapper modelMapper;
 
+    private final UsersEventPublisher usersEventPublisher;
+
+    public UserService(UsersEventPublisher usersEventPublisher)
+    {
+        this.usersEventPublisher = usersEventPublisher;
+    }
+
+    @Transactional
     public UserExtendedDTO createUser(UserFullPasswordDTO user){
         User newUser = modelMapper.map(user, User.class);
 
         User createdUser = userRepository.save(newUser);
         userRepository.flush();
 
-        return modelMapper.map(createdUser, UserExtendedDTO.class);
+        UserExtendedDTO createdDTO = modelMapper.map(createdUser, UserExtendedDTO.class);
+
+        usersEventPublisher.publishUserCreated(new UserCreated(createdDTO));
+
+        return createdDTO;
     }
 
 }
