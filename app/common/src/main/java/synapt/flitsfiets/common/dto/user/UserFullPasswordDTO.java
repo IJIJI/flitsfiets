a1ca2942ae7dd76c19a8f29916e283353fe93584
IJssel <ijssel@synapt.nl>
2025-12-27 19:04:29 +0100

    Added UserFullPasswordDTO

---
+++ app/common/src/main/java/synapt/flitsfiets/common/dto/user/UserFullPasswordDTO.java
@@ -0,0 +1,12 @@
+package synapt.flitsfiets.common.dto.user;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import synapt.flitsfiets.common.enums.UserType;
+import synapt.flitsfiets.common.valueObject.UserAddress;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserFullPasswordDTO extends UserFullDTO{
+    private String password;
+}
