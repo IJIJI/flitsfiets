9256f79c66c3b52025e2d0fff252595cfa27c576
IJssel <ijssel@synapt.nl>
2025-12-27 18:33:21 +0100

    Added placeholder user controller

--- app/services/users/src/main/java/synapt.flitsfiets.users/controller/UserController.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/controller/UserController.java
@@ -1,4 +1,21 @@
 package synapt.flitsfiets.users.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
+import synapt.flitsfiets.common.dto.user.UserFullDTO;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping
+    public UserExtendedDTO createUser(@RequestBody UserFullDTO requestedUser) {
+        System.out.println("User Requested in USER SERVICE:");
+        System.out.println(requestedUser);
+        return null;
+    }
+
 }
