d775eb3780be1e61f9b60dd84f50add1d1d007a4
IJssel <ijssel@synapt.nl>
2025-12-27 21:03:13 +0100

    Added emailExists

--- app/services/users/src/main/java/synapt.flitsfiets.users/controller/UserController.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/controller/UserController.java
@@ -1,10 +1,10 @@
 package synapt.flitsfiets.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import synapt.flitsfiets.common.dto.user.UserExtendedDTO;
 import synapt.flitsfiets.common.dto.user.UserFullPasswordDTO;
 import synapt.flitsfiets.users.service.UserService;
@@ -16,8 +16,18 @@ public class UserController {
     @Autowired
     UserService userService;
 
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<String> unauthorized() {
+        return new ResponseEntity<>("A user already exists by that email address!", HttpStatus.BAD_REQUEST);
+    }
+
     @PostMapping
     public UserExtendedDTO createUser(@RequestBody UserFullPasswordDTO requestedUser) {
+
+        if(userService.emailExists(requestedUser.getEmail())){
+            throw new DuplicateKeyException("A user already exists by that email address!");
+        }
 
         UserExtendedDTO newUser = userService.createUser(requestedUser);
 
