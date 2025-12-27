89973af31b143aee4f13d298a32ad3a05d6d0949
IJssel <ijssel@synapt.nl>
2025-12-27 22:23:07 +0100

    Added subscriptionrepo

--- app/services/users/src/main/java/synapt.flitsfiets.users/service/UserService.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/service/UserService.java
@@ -9,13 +9,19 @@ import synapt.flitsfiets.common.dto.user.UserFullPasswordDTO;
 import synapt.flitsfiets.common.events.users.UserCreated;
 import synapt.flitsfiets.users.messaging.UsersEventPublisher;
 import synapt.flitsfiets.users.model.User;
+import synapt.flitsfiets.users.model.UserSubscriptionPeriod;
 import synapt.flitsfiets.users.repository.UserRepository;
+import synapt.flitsfiets.users.repository.UserSubscriptionRepository;
+
+import java.time.Instant;
 
 @Service
 public class UserService
 {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    UserSubscriptionRepository userSubscriptionRepository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -30,10 +36,23 @@ public class UserService
     public UserExtendedDTO createUser(UserFullPasswordDTO user){
         User newUser = modelMapper.map(user, User.class);
 
+        UserSubscriptionPeriod subscriptionPeriod = new UserSubscriptionPeriod();
+        subscriptionPeriod.setStartDate(Instant.now());
+        subscriptionPeriod.setBike(user.getActiveBikeType());
+        subscriptionPeriod.setPlan(user.getActivePlanType());
+
         User createdUser = userRepository.save(newUser);
         userRepository.flush();
 
+        subscriptionPeriod.setUser(createdUser);
+        userSubscriptionRepository.save(subscriptionPeriod);
+        userSubscriptionRepository.flush();
+
+
         UserExtendedDTO createdDTO = modelMapper.map(createdUser, UserExtendedDTO.class);
+
+        createdDTO.setActivePlanType(subscriptionPeriod.getPlan());
+        createdDTO.setActiveBikeType(subscriptionPeriod.getBike());
 
         usersEventPublisher.publishUserCreated(new UserCreated(createdDTO));
 
