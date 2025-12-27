be5b5ac8f211c614cc2d84519853d2d9fc374d62
IJssel <ijssel@synapt.nl>
2025-12-27 19:03:47 +0100

    Added modelmapper

--- app/services/users/src/main/java/synapt.flitsfiets.users/UsersApplication.java
+++ app/services/users/src/main/java/synapt.flitsfiets.users/UsersApplication.java
@@ -3,11 +3,14 @@ package synapt.flitsfiets.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import synapt.flitsfiets.common.config.ModelMapperConfig;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan(basePackages = "synapt.flitsfiets.common")
+@Import({ModelMapperConfig.class})
 public class UsersApplication {
 
 	public static void main(String[] args) {
