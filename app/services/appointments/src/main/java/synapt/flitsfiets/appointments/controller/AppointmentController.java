7e78d84b1a1b0f4a527cd31216f7bef6ad7837d7
IJssel <ijssel@synapt.nl>
2026-01-02 22:49:59 +0100

    Started appt controller

--- app/services/appointments/src/main/java/synapt/flitsfiets/appointments/controller/AppointmentController.java
+++ app/services/appointments/src/main/java/synapt/flitsfiets/appointments/controller/AppointmentController.java
@@ -1,10 +1,20 @@
 package synapt.flitsfiets.appointments.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import synapt.flitsfiets.appointments.dto.TimeslotDto;
+import synapt.flitsfiets.common.enums.Location;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController
 {
+    @GetMapping
+    public List<TimeslotDto> getPossibleSlots(@RequestParam("loc") Location location){
+        return null;
+    }
 }
