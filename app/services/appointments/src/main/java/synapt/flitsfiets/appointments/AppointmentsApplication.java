d229cc2ad206b97991c97e228a9e269440186e6c
IJssel <ijssel@synapt.nl>
2026-01-02 22:19:11 +0100

    Added moddelmapper

--- app/services/appointments/src/main/java/synapt/flitsfiets/appointments/AppointmentsApplication.java
+++ app/services/appointments/src/main/java/synapt/flitsfiets/appointments/AppointmentsApplication.java
@@ -3,9 +3,12 @@ package synapt.flitsfiets.appointments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
+import synapt.flitsfiets.common.config.ModelMapperConfig;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = "synapt.flitsfiets.common")
+@Import({ModelMapperConfig.class})
 public class AppointmentsApplication {
 
 	public static void main(String[] args) {
