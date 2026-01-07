c5c8fb55bcbf4ad9406ba7f08eaf5b9316834201
IJssel <ijssel@synapt.nl>
2026-01-07 20:22:46 +0100

    Added timeslot DTO to common

---
+++ app/common/src/main/java/synapt/flitsfiets/common/dto/appointment/TimeSlotDTO.java
@@ -0,0 +1,17 @@
+package synapt.flitsfiets.common.dto.appointment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import synapt.flitsfiets.common.enums.Location;
+
+import java.time.Instant;
+
+@Data
+@ToString(callSuper = true)
+@AllArgsConstructor
+public class TimeSlotDTO
+{
+    Instant start;
+    Location location;
+}
