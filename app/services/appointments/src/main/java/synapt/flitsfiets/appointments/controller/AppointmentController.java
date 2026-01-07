96beaa13404b46c134dff83e1c66b939c669b07f
IJssel <ijssel@synapt.nl>
2026-01-07 21:02:11 +0100

    Added AppointmentController test

--- app/services/appointments/src/main/java/synapt/flitsfiets/appointments/controller/AppointmentController.java
+++ app/services/appointments/src/main/java/synapt/flitsfiets/appointments/controller/AppointmentController.java
@@ -1,20 +1,27 @@
 package synapt.flitsfiets.appointments.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import synapt.flitsfiets.appointments.dto.TimeslotDto;
+import synapt.flitsfiets.appointments.util.RosterUtil;
+import synapt.flitsfiets.common.dto.appointment.TimeSlotDTO;
 import synapt.flitsfiets.common.enums.Location;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController
 {
+    @Autowired
+    RosterUtil rosterUtil;
+
     @GetMapping
-    public List<TimeslotDto> getPossibleSlots(@RequestParam("loc") Location location){
-        return null;
+    public List<TimeSlotDTO> getPossibleSlots(@RequestParam("loc") Location location){
+        return rosterUtil.generatePlaceholderRoster(Location.AMSTERDAM);
     }
 }
