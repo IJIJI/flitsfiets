0dba1b3a5d26379366f827ee6c570e28adbdff93
IJssel <ijssel@synapt.nl>
2026-01-07 22:45:57 +0100

    Added actual slots

--- app/services/onboarding/react/src/components/calendar/slotPicker/SlotPicker.tsx
+++ app/services/onboarding/react/src/components/calendar/slotPicker/SlotPicker.tsx
@@ -3,51 +3,11 @@ import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
 
-const testSlots = [
-    {
-        start: "2026-01-08T07:00:00Z",
-        end: "2026-01-08T07:15:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T07:15:00Z",
-        end: "2026-01-08T07:30:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T07:30:00Z",
-        end: "2026-01-08T07:45:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T07:45:00Z",
-        end: "2026-01-08T08:00:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T08:00:00Z",
-        end: "2026-01-08T08:15:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T08:15:00Z",
-        end: "2026-01-08T08:30:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T08:30:00Z",
-        end: "2026-01-08T08:45:00Z",
-        location: "DELFT"
-    },
-    {
-        start: "2026-01-08T08:45:00Z",
-        end: "2026-01-08T09:00:00Z",
-        location: "DELFT"
-    },
-];
 
+export default function SlotPicker({slots}: {slots: {}[]}) {
 
-export default function SlotPicker({slots}: {slots: object|null}) {
+    if (slots == null)
+        slots = [];
 
     return (
         <div className="calendar">
@@ -70,8 +30,7 @@ export default function SlotPicker({slots}: {slots: object|null}) {
                     startTime: '08:00',
                     endTime: '18:00',
                 }}
-                // events={slots}
-                events={testSlots}
+                events={slots}
             />
         </div>
     )
