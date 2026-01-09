603af8e3e7e3cceed3f2cbf8091271593a84843f
IJssel <ijssel@synapt.nl>
2026-01-09 20:33:45 +0100

    Var cleanup

--- app/services/onboarding/react/src/features/register/Register.tsx
+++ app/services/onboarding/react/src/features/register/Register.tsx
@@ -141,8 +141,8 @@ export default function Register() {
 
     const parsePickupCity = (city: string) => {
 
-        var splitStr = city.toLowerCase().split('_');
-        for (var i = 0; i < splitStr.length; i++) {
+        const splitStr = city.toLowerCase().split('_');
+        for (let i = 0; i < splitStr.length; i++) {
             // You do not need to check if i is larger than splitStr length, as your for does that for you
             // Assign it back to the array
             splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
