c15b0b3b6914eafbb97671ccaf7148992c91b035
IJssel <ijssel@synapt.nl>
2026-01-09 23:12:31 +0100

    Added appointment created dto

---
+++ app/common/src/main/java/synapt/flitsfiets/common/events/appointments/AppointmentCreated.java
@@ -0,0 +1,7 @@
+package synapt.flitsfiets.common.events.appointments;
+
+import synapt.flitsfiets.common.dto.appointment.AppointmentDTO;
+
+public record AppointmentCreated (
+        AppointmentDTO user
+) {}
