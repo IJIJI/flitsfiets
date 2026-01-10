b46b1c8ce6db5d73686a623427002096dc25098d
IJssel <ijssel@synapt.nl>
2026-01-10 16:09:37 +0100

    Added check for property loading

--- app/common/src/main/java/synapt/flitsfiets/common/config/rest/AppointmentsRestConfig.java
+++ app/common/src/main/java/synapt/flitsfiets/common/config/rest/AppointmentsRestConfig.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import synapt.flitsfiets.common.config.ServicesProperties;
+
+import java.util.Objects;
 
 @Configuration
 public class AppointmentsRestConfig
 {
     @Bean
-    public RestClient appointmentsRestClient(@Value("${services.appointments.base-url}") String baseUrl)
+    public RestClient appointmentsRestClient(ServicesProperties servicesProperties)
     {
+        var appointments = Objects.requireNonNull(servicesProperties.appointments(), "Missing config: services.appointments");
+        var baseUrl = Objects.requireNonNull(appointments.baseUrl(), "Missing config: services.appointments.base-url");
+
         RestClient.Builder builder = RestClient.builder();
 
         return builder
--- app/common/src/main/java/synapt/flitsfiets/common/config/rest/UsersRestConfig.java
+++ app/common/src/main/java/synapt/flitsfiets/common/config/rest/UsersRestConfig.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import synapt.flitsfiets.common.config.ServicesProperties;
+
+import java.util.Objects;
 
 @Configuration
 public class UsersRestConfig
 {
     @Bean
-    public RestClient usersRestClient(@Value("${services.users.base-url}") String baseUrl)
+    public RestClient usersRestClient(ServicesProperties servicesProperties)
     {
+        var users = Objects.requireNonNull(servicesProperties.users(), "Missing config: services.users");
+        var baseUrl = Objects.requireNonNull(users.baseUrl(), "Missing config: services.users.base-url");
+
         RestClient.Builder builder = RestClient.builder();
 
         return builder
